27d97b9d892c5b682ca7217d3644bfd8e73949bb
liukai <17679237105@163.com>
2026-05-21 09:19:24 +0800

    Initial commit

---
+++ src/main/java/com/air/Aigc4lkApplication.java
@@ -0,0 +1,13 @@
+package com.air;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aigc4lkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aigc4lkApplication.class, args);
+    }
+
+}
