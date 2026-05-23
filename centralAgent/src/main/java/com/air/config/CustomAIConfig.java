e50515aa04e2e967f6ad12f73c7a131c8fff7214
liukai <17679237105@163.com>
2026-05-23 13:49:31 +0800

    rebuild启动配置，接入nacos A2A 协议

---
+++ centralAgent/src/main/java/com/air/config/CustomAIConfig.java
@@ -0,0 +1,22 @@
+package com.air.config;
+
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomAIConfig {
+
+    @Bean
+    DashScopeApi dashScopeApi(@Value("${spring.ai.dashscope.api-key}") String apiKey) {
+        return DashScopeApi.builder().apiKey(apiKey).build();
+    }
+
+    @Bean(name = "dashscopeChatModel")
+    ChatModel dashscopeChatModel(DashScopeApi dashScopeApi) {
+        return DashScopeChatModel.builder().dashScopeApi(dashScopeApi).build();
+    }
+}
